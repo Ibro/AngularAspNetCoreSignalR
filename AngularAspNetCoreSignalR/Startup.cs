48a96a00f00037ca4410b1db5c4218676a234c3e
Ibro <ibro@ibro.me>
2017-11-13 21:13:05 +0100

    Add SignalR to DI container and Middleware pipeline

--- AngularAspNetCoreSignalR/Startup.cs
+++ AngularAspNetCoreSignalR/Startup.cs
@@ -10,6 +10,7 @@ namespace AngularAspNetCoreSignalR
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -19,6 +20,11 @@ namespace AngularAspNetCoreSignalR
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("chat");
+            });
         }
     }
 }

127ddf44c9034e74c75e9e48dfba4e8a5d7c48d4
Ibro <ibro@ibro.me>
2017-11-13 21:11:04 +0100

    Cleanup

--- AngularAspNetCoreSignalR/Startup.cs
+++ AngularAspNetCoreSignalR/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AngularAspNetCoreSignalR
@@ -24,11 +19,6 @@ namespace AngularAspNetCoreSignalR
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
     }
 }

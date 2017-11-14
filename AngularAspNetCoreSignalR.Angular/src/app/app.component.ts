291a003a5ee8e68b8ff00fa7d534eb16308fa793
Ibro <ibro@ibro.me>
2017-11-14 06:51:58 +0100

    Extending AppComponent - add properties for nick, message, messages and HubConnection

--- AngularAspNetCoreSignalR.Angular/src/app/app.component.ts
+++ AngularAspNetCoreSignalR.Angular/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { HubConnection } from '@aspnet/signalr-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent  {
+  private _hubConnection: HubConnection;
+  nick = '';
+  message = '';
+  messages: string[] = [];
 }
